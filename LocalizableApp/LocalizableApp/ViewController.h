dfcbba44725f35e8a0231c102d7bb4a22e7f6b0b
nsmaster <nikolay.shatilo@gmail.com>
2014-02-16 08:48:40 +0200

    App was created

---
+++ LocalizableApp/LocalizableApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  LocalizableApp
+//
+//  Created by Nikolay Shatilo on 16.02.14.
+//  Copyright (c) 2014 Nikolay Shatilo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
